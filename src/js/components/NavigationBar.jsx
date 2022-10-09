5d8f778229e709af2c90ff2df6496126774cdfd0
Coffinon <k.kupiszewski4@gmail.com>
2022-10-09 19:59:36 +0200

    hamburger menu added

--- src/js/components/NavigationBar.jsx
+++ src/js/components/NavigationBar.jsx
@@ -2,11 +2,29 @@ import React from "react"
 import {Link} from "react-router-dom"
 
 function NavigationBar() {
+  const linkStyle = {
+    textDecoration: "none"
+  }
+
+  function toggleHamburger() {
+    document.querySelector(".links").classList.toggle("active")
+    document.querySelector(".hamburger-menu").classList.toggle("active")
+  }
+
   return (
     <nav className="navbar">
-      <Link to="/">Home Page</Link>
-      <Link to="/investments">Investments</Link>
+      <div className="links">
+        <Link to="/" style={linkStyle}><p className="link-text" onClick={toggleHamburger}>Home Page</p></Link>
+        <Link to="/investments" style={linkStyle}><p className="link-text" onClick={toggleHamburger}>Investments</p></Link>
+      </div>
+
       <h1>Crypto Investment App</h1>
+
+      <button className="hamburger-menu" onClick={toggleHamburger}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </button>
     </nav>
   )
 }
